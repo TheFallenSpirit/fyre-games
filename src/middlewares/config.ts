5e0a0b2dba9e2424638c7b64a8da9c7f6301a7f2
Liam L <thefallenspirit@outlook.com>
2026-05-07 06:17:31 -0700

    feat(middlewares): Add username to config middleware

--- src/middlewares/config.ts
+++ src/middlewares/config.ts
@@ -1,18 +1,21 @@
+import { GuildI } from '@/models/Guild.js';
 import { getGuild } from '@/store/guild.js';
 import { colors, isInstalled } from '@fallencodes/seyfert-utils';
 import { createMiddleware } from 'seyfert';
 
 export const defaultConfig = {
     color: colors.green,
-    prefix: '-'
+    prefix: '-',
+    username: 'Fyre Games'
 };
 
 export default createMiddleware<typeof defaultConfig>(async ({ next, context }) => {
-    if (!isInstalled(context)) return next(defaultConfig);
-    const guildConfig = await getGuild(context.guildId!);
+    let guildConfig: GuildI | undefined;
+    if (isInstalled(context)) guildConfig = await getGuild(context.guildId!);
     
     next({
         color: guildConfig?.defaultColor ?? defaultConfig.color,
-        prefix: guildConfig?.prefix ?? defaultConfig.prefix
+        prefix: guildConfig?.prefix ?? defaultConfig.prefix,
+        username: context.client.me.username
     });
 });
