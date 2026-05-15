4e5e34c0a7fcca7027d392868e4ea10bbd16d5d4
Liam L <thefallenspirit@outlook.com>
2026-05-14 20:50:21 -0700

    feat(client): Ignore events and commands starting with an underscore

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,12 @@
 import { Client, ClientOptions } from 'seyfert';
 import _lang, { LangKey, LangProps } from './common/lang.js';
+import { basename } from 'node:path';
 
 export default class FyreClient extends Client<true> {
     constructor(options: ClientOptions) {
         super(options);
+        this.events.filter = (path) => !basename(path).startsWith('_');
+        this.commands.filter = (path) => !basename(path).startsWith('_');
     };
 
     public lang = (key: LangKey, props?: LangProps) => {
